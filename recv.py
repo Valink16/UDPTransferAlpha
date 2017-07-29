2773868b3007e25db1d78a278fd69bdacba64f3f
valuthy2 <valuthyk@gmail.com>
2017-07-29 13:29:24 +0200

    Minor update
    You can now send messages by typing when the 2 parts(sending part and receiving part) are ready

--- recv.py
+++ recv.py
@@ -5,4 +5,12 @@ except:
     log("Failed to load socket")
     exit()
 
-def
+def recv():
+    receiver=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    PORT=int(input("Enter receiver's port: "))
+    receiver.bind(("",PORT))
+    while(True):
+        recved, senderInfo=receiver.recvfrom(1024)
+        print(recved.decode("utf-8"))
+        if(recved.decode("utf-8")=="stop" or recved.decode("utf-8")=="exit"):
+            break
--- send.py
+++ send.py
@@ -1,11 +1,48 @@
 from myLib import *
+import netifaces
 try:
     import socket
 except:
     log("Failed to load socket")
     exit()
 
-def targtInfo():
-    IP=input("Target IP : ")
+def targtInfo(baseIP):
+    if(baseIP=="localhost"):
+        print("IP: {}".format(baseIP))
+        PORT=int(input("Target PORT : "))
+        return (baseIP, PORT)
+    IP=baseIP+input("Target IP : {}".format(baseIP))
     PORT=int(input("Target PORT : "))
     return (IP,PORT)
+
+def send():
+    sender=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    interfaces=netifaces.interfaces()
+
+    with open("sample.txt","rb") as file:
+        sendFile=file.read()
+    for i,a in enumerate(interfaces):
+        print("[{}]{}".format(i,a))
+    print("")
+
+
+
+    chInter=int(input("Chose an interface(enter the number): "))
+    if(interfaces[chInter]=="lo"):
+        ipBase="localhost"
+    else:
+        ipInfos=netifaces.ifaddresses(interfaces[chInter])
+        infos=ipInfos[netifaces.AF_INET]
+        ipBase=infos[0]["broadcast"][:11]
+
+    log(ipBase)
+    trgt=targtInfo(ipBase)
+    log("IP : {}".format(trgt[0]))
+    log("PORT : {}".format(trgt[1]))
+    log("Ready to communicate. Be sure that target is ready")
+
+    while(True):
+        msg=input().encode("utf-8")
+        sender.sendto(msg,trgt)
+        if(msg==b"exit" or msg==b"stop"):
+            break
